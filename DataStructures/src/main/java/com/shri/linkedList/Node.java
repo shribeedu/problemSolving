5cf224c7e038da85c0ee19b4c7796eaccbd8016b
I004839 <shridhara.beedu@sap.com>
2020-03-29 16:26:52 +0530

    Update node to support random pointer

--- DataStructures/src/main/java/com/shri/linkedList/Node.java
+++ DataStructures/src/main/java/com/shri/linkedList/Node.java
@@ -4,12 +4,15 @@ public class Node {
 
     int val;
     Node next;
+    Node rPointer;
 
     Node(int val){
         this.val = val;
         this.next = null;
+        this.rPointer =  null;
     }
 
+    
   /*  public int getValue(){
         return this.val;
     }
